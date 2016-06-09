080cc736854d2e43000455dfe013b399d0df2873
Taworn T <taworn@gmail.com>
2016-06-09 10:25:53 +0700

    - First version.

--- app/src/main/java/diy/uimedia/MainActivity.java
+++ app/src/main/java/diy/uimedia/MainActivity.java
@@ -3,7 +3,9 @@ package diy.uimedia;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,15 +13,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
-    public void onAboutClick(View view) {
-        String message = getResources().getString(R.string.about_dialog_message) + " " + getResources().getString(R.string.version);
-        new AlertDialog.Builder(this)
-                .setTitle(R.string.about_dialog_title)
-                .setMessage(message)
-                .setPositiveButton(R.string.about_dialog_positive, null)
-                .show();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        else if (id == R.id.action_about) {
+            String message = getResources().getString(R.string.about_dialog_message) + " " + getResources().getString(R.string.version);
+            new AlertDialog.Builder(this)
+                    .setTitle(R.string.about_dialog_title)
+                    .setMessage(message)
+                    .setPositiveButton(R.string.about_dialog_positive, null)
+                    .show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
